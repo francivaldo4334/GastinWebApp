8eaaaaa50c8bee4a727a12b70f91efbb3dcfd448
francivaldo4334 <francivaldodev@gmail.com>
2025-06-27 20:49:32 -0300

    :sparkles: feat: add capacitor android

---
+++ gastin/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.fcr.gastin',
+  appName: 'Gastin',
+  webDir: 'dist'
+};
+
+export default config;
