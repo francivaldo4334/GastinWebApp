fcf3f8f83a6b2aa09fa20688d149ba3aa8d07a57
francivaldo4334 <francivaldodev@gmail.com>
2025-06-27 22:03:30 -0300

    :construction: wp

--- gastin/src/presenter/pages/HomePage.tsx
+++ gastin/src/presenter/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import { Component } from "solid-js";
 import { Scaffold } from "../ui/Scaffold";
-
 export const HomePage: Component = () => {
   return <>
     <Scaffold>
-      <Scaffold.AppBar />
+      <Scaffold.AppBar>
+        <Scaffold.AppBar.Actions>
+        </Scaffold.AppBar.Actions>
+      </Scaffold.AppBar>
     </Scaffold>
   </>
 }
--- gastin/src/presenter/ui/Scaffold.tsx
+++ gastin/src/presenter/ui/Scaffold.tsx
@@ -1,16 +1,24 @@
 import { ParentComponent } from "solid-js";
-
-const AppBar: ParentComponent = (props) => {
-  console.log("ok")
-  return <div class="bg-base h-screen w-screen">ok{props.children}</div>
+const Actions: ParentComponent = (props) => {
+  return <div class="flex gap-2 h-14 absolute left-0">{props.children}</div>
 }
 
+interface AppBarComponent extends ParentComponent {
+  Actions: ParentComponent;
+}
+
+const AppBar: AppBarComponent = (props) => {
+  return <div class="">ok{props.children}</div>
+}
+
+AppBar.Actions = Actions
+
 interface ScaffoldComponent extends ParentComponent {
-  AppBar: ParentComponent;
+  AppBar: AppBarComponent;
 }
 
 const Scaffold: ScaffoldComponent = (props) => {
-  return <>{props.children}</>
+  return <div class="h-14 absolute inset-x-0">{props.children}</div>
 }
 
 
