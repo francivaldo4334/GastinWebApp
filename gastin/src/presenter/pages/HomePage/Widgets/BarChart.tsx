d27e4853891a588e96aabd860cc46db2e740de14
francivaldo4334 <francivaldodev@gmail.com>
2025-06-28 22:42:54 -0300

    :construction: wp

--- gastin/src/presenter/pages/HomePage/Widgets/BarChart.tsx
+++ gastin/src/presenter/pages/HomePage/Widgets/BarChart.tsx
@@ -1,33 +1,56 @@
 import { Card } from "@/presenter/ui/Card";
-import { Component, createSignal } from "solid-js";
+import { Component, createMemo, createSelector, createSignal, Match, Switch } from "solid-js";
 import { DefaultChart } from 'solid-chartjs'
 import {
   type ChartData,
   type ChartOptions,
 } from "chart.js";
 import { FormWeekField } from "@/presenter/ui/FormWeekField";
-import { Divider, Flex, IconButton, Menu, MenuContent, MenuTrigger, Spacer, Text } from "@hope-ui/solid";
+import { Divider, Flex, IconButton, Menu, MenuContent, MenuItem, MenuTrigger, Spacer, Text } from "@hope-ui/solid";
 import { MoreVertical } from "lucide-solid";
+import { FormMonthField } from "@/presenter/ui/FormMonthField";
+import { FormSelectField } from "@/presenter/ui/FormSelectField";
 
 export const BarChart: Component = () => {
 
   const [week, setWeek] = createSignal<string>()
+  const [month, setMonth] = createSignal<string>()
+  const [year, setYear] = createSignal<number>(0)
 
-  const data: ChartData<"bar"> = {
-    labels: ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado", "Domingo"],
-    datasets: [
-      {
-        data: [150, 200, 180, 220, 304, 30, 60],
-        backgroundColor: "#60a5fa",
-      },
-    ],
-  };
+  const [typeRange, setTypeRange] = createSignal<"week" | "month" | "year">("week")
+  const typeRangeSelected = createSelector(typeRange)
 
+  const years = [
+    { label: "2025", value: 2025 },
+    { label: "2024", value: 2024 },
+  ]
+
+  const metrics = [
+    { label: "Segunda", value: 150 },
+    { label: "Terça", value: 200 },
+    { label: "Quarta", value: 180 },
+    { label: "Quinta", value: 220 },
+    { label: "Sexta", value: 304 },
+    { label: "Sabado", value: 30 },
+    { label: "Domingo", value: 60 },
+  ]
+
+
+  const data = createMemo(() => {
+    return {
+      labels: metrics.map(it => it.label),
+      datasets: [
+        {
+          data: metrics.map(it => it.value),
+          backgroundColor: "#60a5fa",
+        }
+      ]
+    }
+  })
   const options: ChartOptions<"bar"> = {
     responsive: true,
     plugins: {
       legend: { position: "top" },
-      title: { display: true, text: "Vendas por mês" },
     },
   };
 
@@ -45,6 +68,28 @@ export const BarChart: Component = () => {
           icon={<MoreVertical />}
         />
         <MenuContent>
+
+          <MenuItem
+            onSelect={() => {
+              setTypeRange("week")
+            }}
+          >
+            Por semana
+          </MenuItem>
+          <MenuItem
+            onSelect={() => {
+              setTypeRange("month")
+            }}
+          >
+            Por mês
+          </MenuItem>
+          <MenuItem
+            onSelect={() => {
+              setTypeRange("year")
+            }}
+          >
+            Por ano
+          </MenuItem>
         </MenuContent>
       </Menu>
     </Card.Header>
@@ -58,13 +103,30 @@ export const BarChart: Component = () => {
       <Flex
         width="$sm"
       >
-        <FormWeekField
-          value={week}
-          setValue={setWeek}
-        />
+        <Switch>
+          <Match when={typeRangeSelected("week")}>
+            <FormWeekField
+              value={week}
+              setValue={setWeek}
+            />
+          </Match>
+          <Match when={typeRangeSelected("month")}>
+            <FormMonthField
+              value={month}
+              setValue={setMonth}
+            />
+          </Match>
+          <Match when={typeRangeSelected("year")}>
+            <FormSelectField
+              items={years}
+              value={year}
+              setValue={setYear}
+            />
+          </Match>
+        </Switch>
       </Flex>
       <Divider />
     </Flex>
-    <DefaultChart type="bar" data={data} options={options} height={300} />
+    <DefaultChart type="bar" data={data()} options={options} height={300} />
   </Card>
 }
