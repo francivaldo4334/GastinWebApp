b8b081bf0b62acc66ed09266f58797376951ede1
francivaldo4334 <francivaldodev@gmail.com>
2025-07-01 16:27:43 -0300

    :construction: wp

--- gastinionic/src/router/index.ts
+++ gastinionic/src/router/index.ts
@@ -5,29 +5,41 @@ import TabsPage from '../views/TabsPage.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    component: HomePage
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
+    path: '/help',
+    component: HelpPage,
+  //   children: [
+  //     {
+  //       path: '',
+  //       redirect: '/tabs/tab1'
+  //     },
+  //     {
+  //       path: 'tab1',
+  //       component: () => import('@/views/Tab1Page.vue')
+  //     },
+  //     {
+  //       path: 'tab2',
+  //       component: () => import('@/views/Tab2Page.vue')
+  //     },
+  //     {
+  //       path: 'tab3',
+  //       component: () => import('@/views/Tab3Page.vue')
+  //     }
+  //   ]
+  },
+  {
+    path: "/categories",
+    component: CategoriesPage,
+  },
+  {
+    path: "/expenditures",
+    component: ExpenditurePage,
+  },
+  {
+    path: "/receipts",
+    component: ReceiptsPage,
   }
 ]
 
