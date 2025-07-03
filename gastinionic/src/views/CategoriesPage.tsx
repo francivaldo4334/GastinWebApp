c280d9984b3efe61f8feb88b61ee07ddb62e4a48
francivaldo4334 <francivaldodev@gmail.com>
2025-07-03 08:59:34 -0300

    :construction: wp

--- gastinionic/src/views/CategoriesPage.tsx
+++ gastinionic/src/views/CategoriesPage.tsx
@@ -1,17 +1,28 @@
+import { ModalCategoryForm } from "@/components/ModalCategoryForm";
 import { FactoryRepositoryDomain } from "@/domain/FactoryRepositoryDomain";
 import { CategoryDomainModel } from "@/domain/models/CategoryDomainModel";
-import { IonBackButton, IonButton, IonButtons, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonNote, IonPage, IonTitle, IonToolbar } from "@ionic/vue";
+import { IonBackButton, IonButton, IonButtons, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonModal, IonNote, IonPage, IonTitle, IonToolbar } from "@ionic/vue";
 import { addOutline } from "ionicons/icons";
 import { defineComponent, onMounted, ref } from "vue";
 
 export default defineComponent({
   setup() {
+    const isOpenModalCategory = ref(false)
     const repo = FactoryRepositoryDomain.getRepository("category")
     const categories = ref<CategoryDomainModel[]>([])
+
+    const onCloseModalCategory = () => {
+      isOpenModalCategory.value = false
+    }
+    const onOpenModalCategory = () => {
+      isOpenModalCategory.value = true
+    }
+
     onMounted(async () => {
       const list = await repo.list()
       categories.value = list
     })
+
     return () => (
       <IonPage>
         <IonHeader>
@@ -26,7 +37,9 @@ export default defineComponent({
         </IonHeader>
         <IonContent>
           <IonFab slot="fixed" horizontal="end" vertical="bottom">
-            <IonFabButton>
+            <IonFabButton
+              onClick={onOpenModalCategory}
+            >
               <IonIcon
                 icon={addOutline}
               />
@@ -47,6 +60,15 @@ export default defineComponent({
             }
           </IonList>
         </IonContent>
+        <IonModal
+          isOpen={isOpenModalCategory.value}
+        >
+          <IonContent>
+            <ModalCategoryForm
+              onClose={onCloseModalCategory}
+            />
+          </IonContent>
+        </IonModal>
       </IonPage>
     )
   }
