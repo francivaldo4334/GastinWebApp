b4e382ec9a243f02f5d1985184861e834ba293bb
francivaldo4334 <francivaldodev@gmail.com>
2025-07-03 15:47:41 -0300

    :construction: wp

--- gastinionic/src/components/ChartWidgets.tsx
+++ gastinionic/src/components/ChartWidgets.tsx
@@ -1,11 +1,13 @@
 import { IonContent } from "@ionic/vue";
 import { defineComponent } from "vue";
 import { WidgetAccountBalance } from "./WidgetAccountBalance";
+import { WidgetPieChart } from "./WidgetPieChart";
 
 export const ChartWidgets = defineComponent({
   setup(){
     return()=>(<IonContent class="ion-padding">
       <WidgetAccountBalance/>
+      <WidgetPieChart/>
     </IonContent>)
   }
 })
---
+++ gastinionic/src/components/WidgetPieChart.tsx
@@ -0,0 +1,169 @@
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonIcon, IonItem, IonPopover, IonTitle, IonToolbar } from "@ionic/vue";
+import { ellipsisVerticalOutline } from "ionicons/icons";
+import { defineComponent, ref } from "vue";
+
+export const WidgetPieChart = defineComponent({
+  setup() {
+    const isOpenMoreOptions = ref(false)
+    return () => (<IonCard>
+      <IonToolbar>
+        <IonCardHeader>
+          <IonCardTitle>
+            Despesas por categoria
+          </IonCardTitle>
+        </IonCardHeader>
+        <IonButtons slot="end">
+          <IonButton
+            id="widget-pie-chart-more-options"
+            onClick={() => { isOpenMoreOptions.value = true }}
+          >
+            <IonIcon icon={ellipsisVerticalOutline} />
+          </IonButton>
+          <IonPopover
+            isOpen={isOpenMoreOptions.value}
+            trigger="widget-pie-chart-more-options"
+          >
+            <IonContent
+              onClick={() => { isOpenMoreOptions.value = false }}
+            >
+              <IonItem
+                button
+                routerLink="/categories"
+              >Ver Categorias</IonItem>
+            </IonContent>
+          </IonPopover>
+        </IonButtons>
+      </IonToolbar>
+      <IonCardContent>
+
+      </IonCardContent>
+    </IonCard>)
+  }
+})
+
+//   return <Card>
+//     <Card.Header>
+//       <Text></Text>
+//       <Spacer />
+//       <Menu>
+//         <MenuTrigger
+//           as={IconButton}
+//           variant="ghost"
+//           colorScheme="neutral"
+//           aria-label="More options of pie chart"
+//           icon={<MoreVertical size={20} />}
+//           size="sm"
+//         />
+//         <MenuContent>
+//           <MenuItem
+//             icon={<ListIcon />}
+//             onSelect={() => {
+//               navigate("/categories")
+//             }}
+//           >
+//             Ver categorias
+//           </MenuItem>
+//           <MenuItem
+//             icon={<Tag />}
+//             onSelect={openNewCategory}
+//           >
+//             Adicionar categoria
+//           </MenuItem>
+//         </MenuContent>
+//       </Menu>
+//     </Card.Header>
+//     <ValidityRange
+//       initValidity={initValidity}
+//       setInitValidity={setInitValidity}
+//       endValidity={endValidity}
+//       setEndValidity={setEndValidity}
+//     />
+//     <Grid
+//       templateColumns="repeat(2,1fr)"
+//       gap="$4"
+//       width="$full"
+//       marginTop="$4"
+//     >
+//       <GridItem>
+//         <DefaultChart
+//           type="pie"
+//           data={chartData()}
+//           options={{
+//             responsive: true,
+//             maintainAspectRatio: false,
+//           }}
+//         />
+//       </GridItem>
+//       <GridItem>
+//         <List>
+//           <For
+//             each={pieItems}
+//           >
+//             {item => (
+//               <ListItem
+//                 class="flex w-full justify-between h-10"
+//               >
+//                 <Badge bgColor={item.color} boxSize="$6" />
+//                 <Text>{item.label}</Text>
+//                 <Text>{item.percentage} %</Text>
+//               </ListItem>
+//             )}
+//           </For>
+//         </List>
+//       </GridItem>
+//     </Grid>
+//   </Card>
+// }
+
+
+// import { Component, createMemo, createSignal, For } from "solid-js"
+// import { DefaultChart } from 'solid-chartjs'
+// import { Card } from "@/presenter/ui/Card"
+// import { Badge, Grid, GridItem, IconButton, List, ListItem, Menu, MenuContent, MenuItem, MenuTrigger, Spacer, Text } from "@hope-ui/solid"
+// import { MoreVertical, List as ListIcon, Tag } from "lucide-solid"
+// import { ValidityRange } from "@/presenter/ui/ValidityRange"
+// import { useStore } from "@/presenter/stores/Store"
+// import { useNavigate } from "@solidjs/router"
+//
+// export const PieChart: Component = () => {
+//
+//   const [initValidity, setInitValidity] = createSignal<string>()
+//   const [endValidity, setEndValidity] = createSignal<string>()
+//
+//   const navigate = useNavigate()
+//
+//   const { openNewCategory } = useStore()
+//
+//   const pieItems = [
+//     {
+//       label: "Gastos",
+//       value: 45,
+//       color: "#f87171",
+//       percentage: 45,
+//     },
+//     {
+//       label: "Ganhos",
+//       value: 30,
+//       color: "#34d399",
+//       percentage: 30,
+//     },
+//     {
+//       label: "Poupança",
+//       value: 25,
+//       color: "#60a5fa",
+//       percentage: 25,
+//     },
+//   ]
+//
+//   const chartData = createMemo(() => {
+//     return {
+//       labels: pieItems.map(it => it.label),
+//       datasets: [
+//         {
+//           data: pieItems.map(it => it.value),
+//           backgroundColor: pieItems.map(it => it.color),
+//         }
+//       ]
+//     }
+//   })
+//
