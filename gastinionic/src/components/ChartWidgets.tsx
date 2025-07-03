7788a90d81df1e0dc94a1cfffe75485da0918f36
francivaldo4334 <francivaldodev@gmail.com>
2025-07-03 16:27:19 -0300

    :construction: wp

--- gastinionic/src/components/ChartWidgets.tsx
+++ gastinionic/src/components/ChartWidgets.tsx
@@ -2,12 +2,14 @@ import { IonContent } from "@ionic/vue";
 import { defineComponent } from "vue";
 import { WidgetAccountBalance } from "./WidgetAccountBalance";
 import { WidgetPieChart } from "./WidgetPieChart";
+import { WidgetBarChart } from "./WidgetBarChart";
 
 export const ChartWidgets = defineComponent({
-  setup(){
-    return()=>(<IonContent class="ion-padding">
-      <WidgetAccountBalance/>
-      <WidgetPieChart/>
+  setup() {
+    return () => (<IonContent class="ion-padding">
+      <WidgetAccountBalance />
+      <WidgetPieChart />
+      <WidgetBarChart />
     </IonContent>)
   }
 })
